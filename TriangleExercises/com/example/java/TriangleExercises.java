3a4cfa9886a521a5db4ed28e8b3f9636257cd12a
Unicornelia <ez.kornelia@gmail.com>
2017-02-16 16:44:29 +0000

    Adds first task from triangle exercises, TWU assignment

---
+++ TriangleExercises/com/example/java/TriangleExercises.java
@@ -0,0 +1,7 @@
+class TriangleExercises {
+  String star = "*";
+
+  void asterisk() {
+    System.out.println(star);
+  }
+}
---
+++ TriangleExercises/com/example/java/TriangleExercisesTestDrive.java
@@ -0,0 +1,6 @@
+class TriangleExercisesTestDrive {
+  public static void main (String[] args) {
+    TriangleExercises task1 = new TriangleExercises();
+    task1.asterisk();
+  }
+}
