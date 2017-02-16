1f2ebedb1f5efcb99ea86ccc3f8ea11a7d57aabe
Unicornelia <ez.kornelia@gmail.com>
2017-02-16 17:30:10 +0000

    Added task two, and updated previous method to be more explicit

--- TriangleExercises/com/example/java/TriangleExercises.java
+++ TriangleExercises/com/example/java/TriangleExercises.java
@@ -2,6 +2,15 @@ class TriangleExercises {
   String star = "*";
 
   void asterisk() {
+    System.out.println("First task: ");
     System.out.println(star);
+  }
+
+  void printMultipleAsterisks(int n) {
+    System.out.println("Second task: ");
+
+    for (int i = 0; i <= n; i ++) {
+      System.out.print(star);
+    }
   }
 }
--- TriangleExercises/com/example/java/TriangleExercisesTestDrive.java
+++ TriangleExercises/com/example/java/TriangleExercisesTestDrive.java
@@ -1,6 +1,10 @@
 class TriangleExercisesTestDrive {
   public static void main (String[] args) {
+
     TriangleExercises task1 = new TriangleExercises();
+    TriangleExercises task2 = new TriangleExercises();
+
     task1.asterisk();
+    task2.printMultipleAsterisks(8);
   }
 }
