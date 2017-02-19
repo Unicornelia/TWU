9b4871073d959e3494553c05531b32b02cfe8905
Unicornelia <ez.kornelia@gmail.com>
2017-02-19 17:07:16 +0000

    Fourth triangle task finished

--- TriangleExercises/com/example/java/TriangleExercises.java
+++ TriangleExercises/com/example/java/TriangleExercises.java
@@ -2,12 +2,12 @@ class TriangleExercises {
   String star = "*";
 
   void asterisk() {
-    System.out.println("First task: ");
+    System.out.println("First task: print asterisk");
     System.out.println(star);
   }
 
   void horizontalLine(int n) {
-    System.out.println("Second task: ");
+    System.out.println("Second task: horizontal line");
 
     for (int i = 0; i < n; i ++) {
       System.out.print(star);
@@ -16,10 +16,21 @@ class TriangleExercises {
   }
 
   void verticalLine(int n) {
-    System.out.println("Third task: ");
+    System.out.println("Third task: vertical line");
 
     for (int i = 0; i < n; i ++) {
       System.out.println(star);
+    }
+  }
+
+  void rightTriangle(int n) {
+    System.out.println("Fourth task: right triangle");
+
+    for (int i = 0; i < n; i ++) {
+      for (int j = 0; j <= i; j ++) {
+        System.out.print(star);
+      }
+      System.out.println("");
     }
   }
 }
--- TriangleExercises/com/example/java/TriangleExercisesTestDrive.java
+++ TriangleExercises/com/example/java/TriangleExercisesTestDrive.java
@@ -4,9 +4,11 @@ class TriangleExercisesTestDrive {
     TriangleExercises task1 = new TriangleExercises();
     TriangleExercises task2 = new TriangleExercises();
     TriangleExercises task3 = new TriangleExercises();
+    TriangleExercises task4 = new TriangleExercises();
 
     task1.asterisk();
     task2.horizontalLine(8);
     task3.verticalLine(3);
+    task4.rightTriangle(3);
   }
 }
